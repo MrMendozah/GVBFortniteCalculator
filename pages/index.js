1205e99dda805ff8322fb6b503a1e8fc137e4f69
MrMendozah <juanmendoza01011@gmail.com>
2026-01-06 14:59:11 -0500

    Add Home component for GVB Fortnite Calculator

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>GVB Fortnite Calculator</h1>
+      <p>Welcome to the calculator!</p>
+    </div>
+  )
+}
